2010bf3495b19191eaf4351e35e48fa78e77b0d9
cainianwei <27919507@qq.com>
2020-09-19 14:32:34 +0800

    完成框架初步搭建

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-      登录组件aaa
+  <div class="login_center">
+      <div class="login_box">
+
+      </div>
   </div>
 </template>
 
@@ -10,6 +12,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.login_center {background: #2b4b6b; height: 100%; position: relative;}
+.login_box {width: 450px; height: 300px; background: #fff; position: absolute; margin: auto;  top: 0;left: 0;right: 0;bottom: 0;}
 
-</style>
+</style>>
